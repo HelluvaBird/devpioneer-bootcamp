1f0a1faae6edcb4672ad2e9f9e023da2adbbcedd
HelluvaBird <royceman24@hotmail.com>
2024-02-10 16:41:37 -0500

    add metrics section

---
+++ src/lib/components/Metrics.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+</script>
+
+<div class="relative bg-gray-900">
+	<div class="h-80 absolute inset-x-0 bottom-0 xl:top-0 xl:h-full">
+		<div class="h-full w-full xl:grid xl:grid-cols-2">
+			<div class="h-full xl:relative xl:col-start-2">
+				<img
+					src="/images/hero2.jpg"
+					alt="a classroom full of hard working students"
+					class="h-full w-full object-cover opacity-25 grayscale xl:absolute xl:inset-0"
+				/>
+				<div
+					aria-hidden="true"
+					class="absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-gray-900 xl:inset-y-0 xl:left-0 xl:h-full xl:w-32 xl:bg-gradient-to-r"
+				></div>
+			</div>
+		</div>
+	</div>
+	<div
+		class="max-w-4xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 xl:grid xl:grid-cols-2 xl:grid-flow-col-dense xl:gap-x-8"
+	>
+		<div class="relative pt-12 pb-64 sm:pt-24 sm:pb-64 xl:col-start-1 xl:pb-24">
+			<h2 class="text-sm font-semibold tracking-wide uppercase">
+				<span class="bg-gradient-to-r from-purple-300 to-indigo-300 bg-clip-text text-transparent"
+					>Blaze your trail</span
+				>
+			</h2>
+			<p class="mt-3 font-extrabold text-3xl text-white">On a mission to empower developers</p>
+			<p class="mt-5 text-lg text-gray-300">
+				Stay committed to continuous learning and professional development. Keep up with
+				advancements in technology, industry trends, and best practices by reading books, attending
+				workshops and conferences, taking online courses, and participating in relevant communities
+				and forums.
+			</p>
+			<div class="mt-10">
+				<dl class="grid grid-cols-2 gap-x-4 gap-y-8">
+					<div class="border-t-2 border-gray-100 pt-6">
+						<dt class="text-base text-gray-400 font-medium">Founded</dt>
+						<dd class="text-3xl font-extrabold text-white tracking-tight">2024</dd>
+					</div>
+					<div class="border-t-2 border-gray-100 pt-6">
+						<dt class="text-base text-gray-400 font-medium">Employees</dt>
+						<dd class="text-3xl font-extrabold text-white tracking-tight">7</dd>
+					</div>
+					<div class="border-t-2 border-gray-100 pt-6">
+						<dt class="text-base text-gray-400 font-medium">Students</dt>
+						<dd class="text-3xl font-extrabold text-white tracking-tight">74</dd>
+					</div>
+					<div class="border-t-2 border-gray-100 pt-6">
+						<dt class="text-base text-gray-400 font-medium">Graduation Rate*</dt>
+						<dd class="text-3xl font-extrabold text-white tracking-tight">TBD</dd>
+					</div>
+				</dl>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,12 @@
 	import Features from '$lib/components/Features.svelte';
 	import Hero from '$lib/components/Hero.svelte';
 	import LogoCloud from '$lib/components/LogoCloud.svelte';
+	import Metrics from '$lib/components/Metrics.svelte';
 </script>
 
 <main>
 	<Hero />
 	<LogoCloud />
 	<Features />
+	<Metrics />
 </main>
