7e68f7317da7d3a6005d90587fc1de62182b7a3e
HelluvaBird <royceman24@hotmail.com>
2024-02-20 19:59:15 -0500

    continue adding information to the program routes

--- src/routes/programs/full-time/+page.svelte
+++ src/routes/programs/full-time/+page.svelte
@@ -12,6 +12,29 @@
 
 	let currentTab = 0;
 	const setCurrentTab = (x: number) => (currentTab = x);
+	const tabs = [
+		{
+			title: 'Foundations',
+			description:
+				'The Foundations unit consists of lessons on HTML, CSS, and Javascript. These are the core technologies used in modern web development. These lessons are designed to help all pioneers of any skill level. The Foundations curriculum covers the basics of programming through the more advanced concepts.',
+			curriculum: [
+				'HTML - the foundation of web development',
+				'CSS - making your web sites look great',
+				'Javascript - giving functionality to your web sites',
+				'Git - Version Control System to save your progress'
+			],
+			features: [
+				'Supports pioneers of all skill levels',
+				'Hands-on learning approach. Learn by practicing your skills.',
+				'Industry-experienced instructors',
+				'Immersive learning experience',
+				'Unit concludes with a wrap-up project to showcase your skills'
+			]
+		},
+		{ title: 'Front-end Development', description: '', curriculum: [], features: [] },
+		{ title: 'Back-end Development', description: '', curriculum: [], features: [] },
+		{ title: 'Full Stack Development', description: '', curriculum: [], features: [] }
+	];
 </script>
 
 <main>
@@ -243,54 +266,180 @@
 									Classes are held <span class="font-bold">every weekday</span>, Monday through
 									Friday, from 10:00am - 5:00pm
 								</p>
-								<div class="border border-gray-200 rounded-md">
-									<div class="sm:hidden">
+								<div class="border border-gray-200 rounded-md shadow-sm">
+									<div class="sm:hidden p-2">
 										<label for="tabs" class="sr-only">Select a tab</label>
-										<select name="tabs" id="tabs" class="block w-full">
-											<option>Foundations</option>
-											<option>Front-end Development</option>
-											<option>Back-end Development</option>
-											<option>Full Stack Development</option>
-										</select>
+										<div class="relative grid grid-cols-1 items-center">
+											<select
+												on:change={(e) => setCurrentTab(Number(e.currentTarget.value))}
+												bind:value={currentTab}
+												name="tabs"
+												id="tabs"
+												class="col-start-1 block w-full appearance-none bg-none border border-gray-200 rounded-md py-4 px-2 focus:outline-none focus:border-indigo-700"
+											>
+												<option value={0}>Foundations</option>
+												<option value={1}>Front-end Development</option>
+												<option value={2}>Back-end Development</option>
+												<option value={3}>Full Stack Development</option>
+											</select>
+											<span class="absolute col-start-1 right-2 pointer-events-none">
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													fill="none"
+													viewBox="0 0 24 24"
+													stroke-width="1.5"
+													stroke="currentColor"
+													class="w-6 h-6"
+												>
+													<path
+														stroke-linecap="round"
+														stroke-linejoin="round"
+														d="m19.5 8.25-7.5 7.5-7.5-7.5"
+													/>
+												</svg>
+											</span>
+										</div>
 									</div>
 									<div class="hidden sm:block">
 										<div class="rounded-t-md bg-gray-50">
-											<nav class="flex" aria-label="Tabs">
+											<nav class="flex border-b border-gray-200" aria-label="Tabs">
 												<button
 													on:click={() => setCurrentTab(0)}
 													type="button"
 													class="w-0 flex-1 py-4 px-1 border-b-2"
-													class:border-gray-200={currentTab !== 0}
+													class:border-transparent={currentTab !== 0}
 													class:border-indigo-700={currentTab === 0}>Foundations</button
 												>
 												<button
 													type="button"
 													class="w-0 flex-1 py-4 px-1 border-b-2"
+													class:border-transparent={currentTab !== 1}
 													class:border-indigo-700={currentTab === 1}
-													class:border-gray-200={currentTab !== 1}
 													on:click={() => setCurrentTab(1)}>Front-end Development</button
 												>
 												<button
 													type="button"
 													class="w-0 flex-1 py-4 px-1 border-b-2"
 													class:border-indigo-700={currentTab === 2}
-													class:border-gray-200={currentTab !== 2}
+													class:border-transparent={currentTab !== 2}
 													on:click={() => setCurrentTab(2)}>Back-end Development</button
 												>
 												<button
 													type="button"
 													class="w-0 flex-1 py-4 px-1 border-b-2"
 													class:border-indigo-700={currentTab === 3}
-													class:border-gray-200={currentTab !== 3}
+													class:border-transparent={currentTab !== 3}
 													on:click={() => setCurrentTab(3)}>Full Stack Development</button
 												>
 											</nav>
 										</div>
 									</div>
-									<div class="p-6">
-										<p class="text-3xl">We want this to always show</p>
+									<div class="p-6 space-y-6">
+										<p class="text-2xl">{tabs[currentTab].title}</p>
+										<p class="text-base">{tabs[currentTab].description}</p>
+										<p class="text-lg font-semibold">
+											{tabs[currentTab].title} curriculum includes:
+										</p>
+										<ul class="space-y-6">
+											{#each tabs[currentTab].curriculum as item}
+												<li class="flex gap-2">
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														fill="none"
+														viewBox="0 0 24 24"
+														stroke-width="1.5"
+														stroke="currentColor"
+														class="w-6 h-6"
+													>
+														<path
+															stroke-linecap="round"
+															stroke-linejoin="round"
+															d="m4.5 12.75 6 6 9-13.5"
+														/>
+													</svg>
+													<p>{item}</p>
+												</li>
+											{/each}
+										</ul>
+										<p class="text-lg font-semibold">
+											{tabs[currentTab].title} features:
+										</p>
+										<ul class="space-y-6">
+											{#each tabs[currentTab].features as item}
+												<li class="flex gap-2">
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														fill="none"
+														viewBox="0 0 24 24"
+														stroke-width="1.5"
+														stroke="currentColor"
+														class="w-6 h-6"
+													>
+														<path
+															stroke-linecap="round"
+															stroke-linejoin="round"
+															d="m4.5 12.75 6 6 9-13.5"
+														/>
+													</svg>
+													<p>{item}</p>
+												</li>
+											{/each}
+										</ul>
 									</div>
 								</div>
+							</div>
+						</div>
+						<hr class="border-0 h-px bg-gray-200 my-6" />
+						<div class="py-16">
+							<div class="max-w-3xl space-y-6">
+								<h2 class="text-3xl sm:text-4xl text-indigo-700 font-extrabold">Tuition</h2>
+								<p>
+									At DevPioneer Institute we strive to make our education more accessible to our
+									pioneers.
+								</p>
+								<div class="bg-white shadow overflow-hidden rounded-md">
+									<ul role="list" class="divide-y divide-gray-200">
+										<div class="px-6 py-4">
+											<p class="text-lg">
+												The full-time program tuition is <span
+													class="text-lg font-semibold text-indigo-700 tracking-tight">$22,000</span
+												>
+											</p>
+											<p class="text-sm mt-1 text-gray-400 italic max-w-xl">
+												*A refundable deposit of $99, applied to your total tuition, is due at the
+												time of enrollment. The deposit is refundable up until the end of the first
+												week of classes.*
+											</p>
+										</div>
+										<div class="px-6 py-4">
+											<p class="text-lg">
+												There are a number of ways a pioneer can decide to fund their future career
+												in tech. Ways include self-payments, employer-payments, bank loans,
+												scholarships, and educational grants
+											</p>
+										</div>
+										<div class="px-6 py-4 space-y-1">
+											<p class="text-lg">Payment Plans</p>
+											<p class="text-base">As Low As</p>
+											<p class="text-xl font-bold">$300/month</p>
+										</div>
+										<div class="px-6 py-4">
+											<a
+												href="#"
+												class="text-indigo-700 hover:text-indigo-600 font-medium underline"
+												>View all payment options</a
+											>
+										</div>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<hr class="border-0 h-px bg-gray-200 my-6" />
+						<div class="py-16">
+							<div class="max-w-3xl">
+								<h2 class="text-3xl sm:text-4xl text-indigo-700 font-extrabold">
+									Hands-on Projects
+								</h2>
 							</div>
 						</div>
 					</div>
