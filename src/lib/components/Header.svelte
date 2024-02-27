f8bcafc45cf8fd56a818618a8154cf947aa5ebc7
HelluvaBird <royceman24@hotmail.com>
2024-02-26 21:10:01 -0500

    create sign in route page

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -113,7 +113,8 @@
 				</div>
 			</div>
 			<div class="sm:ml-10 space-x-4">
-				<a href="#" class="text-base font-medium text-gray-400 hover:text-gray-900">Sign in</a>
+				<a href="/signin" class="text-base font-medium text-gray-400 hover:text-gray-900">Sign in</a
+				>
 				<a
 					href="/admissions"
 					class="inline-block bg-indigo-600 text-white py-2 px-4 border border-transparent rounded-md text-base font-medium hover:bg-indigo-700/75"
---
+++ src/routes/signin/+page.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+</script>
+
+<main class="flex flex-col justify-center px-4 sm:px-6 lg:px-8 py-16 sm:py-32 bg-gray-200">
+	<div class="max-w-md mx-auto">
+		<h1 class="text-gray-900 text-center">
+			<span class="sr-only" aria-hidden="true">DevPioneer Institute</span>
+			<svg
+				fill="currentColor"
+				stroke-width="1.5"
+				stroke="currentColor"
+				version="1.1"
+				xmlns="http://www.w3.org/2000/svg"
+				xmlns:xlink="http://www.w3.org/1999/xlink"
+				viewBox="0 0 512 512"
+				xml:space="preserve"
+				class="h-12 w-12 inline"
+			>
+				<g>
+					<g>
+						<path
+							d="M510.565,323.799c-2.62-3.925-7.91-4.983-11.836-2.364l-23.45,15.633h-84.727c-1.672-7.33-4.378-14.259-7.987-20.634
+			l24.533-24.533c1.596-1.596,2.5-3.772,2.5-6.033v-33.365l16.922-93.116c0.614-3.345-0.828-6.741-3.661-8.627l-102.4-68.267
+			c-1.391-0.922-3.046-1.425-4.727-1.425h-204.8c-1.681,0-3.337,0.503-4.736,1.434l-102.4,68.267
+			c-2.833,1.886-4.275,5.282-3.652,8.627l16.922,93.107v33.365c0,2.261,0.896,4.437,2.5,6.033l24.533,24.533
+			c-6.315,11.153-9.967,23.996-9.967,37.7c0,42.342,34.449,76.8,76.8,76.8c42.351,0,76.8-34.458,76.8-76.8h51.2
+			c0,42.342,34.449,76.8,76.8,76.8s76.8-34.458,76.8-76.8h85.333c1.681,0,3.336-0.503,4.736-1.434l25.6-17.067
+			C512.126,333.023,513.184,327.724,510.565,323.799z M320.937,103.332l87.791,58.53l-14.78,81.34H284.133L320.937,103.332z
+			 M304.664,98.135l-38.178,145.067H160.169l-38.17-145.067H304.664z M17.927,161.862l87.799-58.53l36.804,139.87H32.715
+			L17.927,161.862z M51.881,362.668h38.451l-27.17,27.17C57.308,382.03,53.332,372.763,51.881,362.668z M51.881,345.602
+			c1.451-10.095,5.427-19.362,11.281-27.17l27.17,27.17H51.881z M102.398,413.186c-10.095-1.451-19.362-5.427-27.17-11.281
+			l27.17-27.17V413.186z M102.398,333.535l-27.17-27.17c7.808-5.854,17.075-9.83,27.17-11.281V333.535z M119.465,413.186v-38.451
+			l27.17,27.17C138.827,407.758,129.56,411.735,119.465,413.186z M158.701,389.838l-27.17-27.17h38.451
+			C168.532,372.763,164.555,382.03,158.701,389.838z M119.465,333.535v-38.451c10.095,1.451,19.362,5.427,27.17,11.281
+			L119.465,333.535z M131.531,345.602l27.17-27.17c5.854,7.808,9.822,17.075,11.281,27.17H131.531z M256.681,362.668h38.451
+			l-27.17,27.17C262.108,382.03,258.132,372.763,256.681,362.668z M256.681,345.602c1.451-10.095,5.427-19.362,11.281-27.17
+			l27.17,27.17H256.681z M307.198,413.186c-10.095-1.451-19.362-5.427-27.17-11.281l27.17-27.17V413.186z M307.198,333.535
+			l-27.17-27.17c7.817-5.854,17.075-9.822,27.17-11.281V333.535z M240.911,337.068h-55.159c-7.791-34.15-38.349-59.733-74.82-59.733
+			c-22.443,0-42.607,9.737-56.661,25.139l-20.139-20.139v-22.067h358.4v22.067l-20.139,20.139
+			c-14.054-15.403-34.219-25.139-56.661-25.139C279.252,277.335,248.702,302.918,240.911,337.068z M363.501,318.431
+			c5.854,7.808,9.822,17.075,11.281,27.17h-38.451L363.501,318.431z M324.265,333.535v-38.451
+			c10.095,1.451,19.362,5.427,27.17,11.281L324.265,333.535z M324.265,413.186v-38.451l27.17,27.17
+			C343.627,407.758,334.36,411.735,324.265,413.186z M363.501,389.838l-27.17-27.17h38.451
+			C373.332,372.763,369.355,382.03,363.501,389.838z"
+						/>
+					</g>
+				</g>
+			</svg>
+		</h1>
+		<p class="mt-6 text-center text-3xl text-gray-900 font-extrabold">Sign in to your account</p>
+		<p class="mt-2 text-center text-sm text-gray-600">
+			Or
+			<a href="/admissions" class="font-medium text-indigo-600 hover:text-indigo-500"
+				>visit the admissions page now</a
+			>
+		</p>
+	</div>
+	<div class="mt-8 mx-auto max-w-md w-full">
+		<div class="bg-white py-8 px-4 shadow rounded-lg sm:px-10">
+			<form class="space-y-6">
+				<div>
+					<label for="email">Email address</label>
+					<div class="mt-1">
+						<input
+							type="email"
+							name="email"
+							id="email"
+							class=" appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+						/>
+					</div>
+				</div>
+				<div>
+					<label for="password">Password</label>
+					<div class="mt-1">
+						<input
+							type="password"
+							name="password"
+							id="password"
+							class=" appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+						/>
+					</div>
+				</div>
+				<div class="flex justify-between items-center">
+					<div class="flex items-center">
+						<input
+							type="checkbox"
+							name="remember"
+							id="remember"
+							class="size-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+						/>
+						<label for="remember" class="ml-2 block text-sm text-gray-900">Remember Me</label>
+					</div>
+					<div class="text-sm">
+						<button type="button" class="font-medium text-indigo-600 hover:text-indigo-500"
+							>Forget your password?</button
+						>
+					</div>
+				</div>
+				<div class="grid">
+					<button
+						type="button"
+						class="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+						>Sign in</button
+					>
+				</div>
+			</form>
+		</div>
+	</div>
+</main>
